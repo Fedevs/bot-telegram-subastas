435f2240b72bb92c2185d1df78bff9c04d6718ad
Fedevs <fededevsit@gmail.com>
2025-01-28 20:20:11 -0300

    env naming

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder
-from config import TEST_TELEGRAM_BOT_TOKEN
+from config import TELEGRAM_BOT_TOKEN
 from bot.commands import setup_commands
 
 logging.basicConfig(
@@ -10,7 +10,7 @@ logging.basicConfig(
 )
 
 def main():
-    application = ApplicationBuilder().token(TEST_TELEGRAM_BOT_TOKEN).build()
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     setup_commands(application)
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
