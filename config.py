9c790a38b3bb9ffc42521491e4a963f1e3b14006
Fedevs <fededevsit@gmail.com>
2025-01-28 14:45:45 -0300

    env changes

--- config.py
+++ config.py
@@ -3,7 +3,14 @@ import os
 
 load_dotenv() 
 
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+DEBUG = os.getenv('DEBUG')
+TELEGRAM_BOT_TOKEN = ''
+if DEBUG:
+    TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN_DEV')
+else:
+    TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+    
 APPWRITE_ENDPOINT = os.getenv('APPWRITE_ENDPOINT')
 APPWRITE_PROJECT_ID = os.getenv('APPWRITE_PROJECT_ID')
 APPWRITE_API_KEY = os.getenv('APPWRITE_API_KEY')
